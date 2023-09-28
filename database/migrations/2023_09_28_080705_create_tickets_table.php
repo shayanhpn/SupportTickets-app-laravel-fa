587bcbe6893fc7a58c013f077c03e88033781645
Shayan HosseinPoorian <shayanhpn@gmail.com>
2023-09-28 11:45:16 +0330

    app created

--- database/migrations/2023_09_28_080705_create_tickets_table.php
+++ database/migrations/2023_09_28_080705_create_tickets_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tickets', function (Blueprint $table) {
             $table->id();
             $table->string('firstname');
-            $table->string('lastname');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('email');
+            $table->string('title');
+            $table->string('section');
+            $table->string('priority');
+            $table->text('message');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tickets');
     }
 };
